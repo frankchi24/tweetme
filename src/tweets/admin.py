7ace303532bd505839e2fcc2854183d39c27391d
Frank <frankchi24>
2017-04-19 16:28:33 +0800

    Model forms

--- src/tweets/admin.py
+++ src/tweets/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Tweet
+from .forms import TweetModelForm
+
+class TweetModelAdmin(admin.ModelAdmin):
+    form = TweetModelForm
+
+
 
 # Register your models here.
-admin.site.register(Tweet)
+admin.site.register(Tweet,TweetModelAdmin)
